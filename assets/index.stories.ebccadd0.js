b526f309779858c6c686ff628cc6542477d4a848
makeqiuqiu <makeqiuqiu@users.noreply.github.com>
2022-09-20 03:02:34 +0000

    deploy: 5080cb01152234604a946d3ec6e3cb88e92a41a6

---
+++ assets/index.stories.ebccadd0.js
@@ -0,0 +1,39 @@
+import{u as c,a as u}from"./index.e1313646.js";import{j as e}from"./jsx-runtime.4144e1e4.js";import"./iframe.52a076aa.js";function n(t){const{children:a,size:s,type:r}=t,o=u("button"),l=c([o.b(),o.useIs(s),o.useIs(r)]);return e("button",{type:"button",className:l,children:a})}const f={parameters:{storySource:{source:`import Button from './index';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['default', 'plain', 'text'],
+      defaultValue: 'default',
+      table: { category: 'base' },
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'default', 'large'],
+      defaultValue: 'default',
+      table: { category: 'base' },
+    },
+    children: {
+      control: 'text',
+      defaultValue: 'Button',
+      table: { category: 'base' },
+    },
+    // color: { control: 'color', table: { category: 'custom' } },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+function Template(args) {
+  return <Button {...args} />;
+}
+
+export const Normal = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Normal.args = {};
+`,locationsMap:{normal:{startLoc:{col:22,line:35},endLoc:{col:39,line:35},startBody:{col:22,line:35},endBody:{col:39,line:35}}}}},title:"Components/Button",component:n,argTypes:{type:{control:"select",options:["default","plain","text"],defaultValue:"default",table:{category:"base"}},size:{control:"select",options:["small","default","large"],defaultValue:"default",table:{category:"base"}},children:{control:"text",defaultValue:"Button",table:{category:"base"}}}};function i(t){return e(n,{...t})}const p=i.bind({});p.args={};const b=["Normal"];export{p as Normal,b as __namedExportsOrder,f as default};
+//# sourceMappingURL=index.stories.ebccadd0.js.map
