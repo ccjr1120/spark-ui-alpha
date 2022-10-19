9c5a6876d95610ff3f8e8d0cad4adb7e9abdaebd
makeqiuqiu <makeqiuqiu@users.noreply.github.com>
2022-10-19 06:52:25 +0000

    deploy: f51015facae6029b80cd396248843fc3cf8cd6a3

--- assets/index.9c13e268.js
+++
@@ -1,2 +0,0 @@
-import{u as c,a as r}from"./index.7c0ea6c3.js";import{j as m}from"./jsx-runtime.43c3873c.js";function b(t){const{children:e,size:o,type:n,...a}=t,s=r("button"),u=c([s.b(),s.useIs(o),s.useIs(n)]);return m("button",{type:"button",className:u,...a,children:e})}export{b as B};
-//# sourceMappingURL=index.9c13e268.js.map
